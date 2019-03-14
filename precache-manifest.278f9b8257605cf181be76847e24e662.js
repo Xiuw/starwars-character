ae0e6d5c14c21ddbb560f7a7201b99fdbacd5f50
xiu wang <sylvi.xw@gmail.com>
2019-03-14 11:11:00 -0400

    Updates

---
+++ precache-manifest.278f9b8257605cf181be76847e24e662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e03336767f8793a3f55",
+    "url": "/star-wars/static/js/runtime~main.0e033367.js"
+  },
+  {
+    "revision": "f04c406b3758dec2c059",
+    "url": "/star-wars/static/js/main.f04c406b.chunk.js"
+  },
+  {
+    "revision": "ad2805ef0ec24a04cbb7",
+    "url": "/star-wars/static/js/2.ad2805ef.chunk.js"
+  },
+  {
+    "revision": "f04c406b3758dec2c059",
+    "url": "/star-wars/static/css/main.373be811.chunk.css"
+  },
+  {
+    "revision": "ad2805ef0ec24a04cbb7",
+    "url": "/star-wars/static/css/2.f6fe376b.chunk.css"
+  },
+  {
+    "revision": "119388b0b562fb41ae2d00a8232664a8",
+    "url": "/star-wars/index.html"
+  }
+];
